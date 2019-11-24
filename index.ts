54540b5abeb04864d6fca1baa9d4622adb960f0a
unknown <rmtestard@gmail.com>
2019-11-24 18:11:56 +0100

    1st

---
+++ index.ts
@@ -0,0 +1,44 @@
+// function isAnagram(stringA: string, stringB: string): boolean {
+//   const sanatizeString = function(str: string): string {
+//     return str
+//       .toLowerCase()
+//       .replace(/[^a-z][^0-9]/g, "")
+//       .split("")
+//       .sort()
+//       .join("");
+//   };
+
+//   return sanatizeString(stringA) === sanatizeString(stringB);
+// }
+
+// function isAnagram(stringA: string, stringB: string): void {
+//   function createCharMap(text: string) {
+//     let charMap = {};
+
+//     for (let char of text) {
+//       if (charMap.hasOwnProperty()) {
+//         charMap[char]++;
+//       } else {
+//         charMap[char] = 1;
+//       }
+//     }
+
+//     return charMap;
+//   }
+
+//   if (stringA.length === stringB.length) {
+//     let stringAMap = createCharMap(stringA);
+//     let stringBMap = createCharMap(stringB);
+
+//     for (let char in stringAMap) {
+//       if (stringAMap[char] !== stringBMap[char]) {
+//         return false;
+//       }
+//       return true;
+//     }
+//   } else {
+//     return false;
+//   }
+// }
+
+console.log(isAnagram("silent", "listen"));
